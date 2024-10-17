035f23bcc8ced39cd1ea5f69f98b567d0c59a00b
wraphx <tyrone@wraphx.com>
2024-10-17 15:52:58 -0400

    Added 5 new wraps

--- src/vehicles/TacomaModel.jsx
+++ src/vehicles/TacomaModel.jsx
@@ -65,7 +65,7 @@ console.log()
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Body.geometry}>
-        <meshStandardMaterial {...materials.BaseMtl} color={overlay==1 || overlay==13 ? (snap1.base.BaseMtl!=="none" ? snap1.base.BaseMtl : 0xffffff) : 0xffffff}>
+        <meshStandardMaterial {...materials.BaseMtl} color={overlay==1 || overlay==13 || overlay==34 ? (snap1.base.BaseMtl!=="none" ? snap1.base.BaseMtl : 0xffffff) : 0xffffff}>
           <canvasTexture
             flipY={false}
             wrapS={1000}
